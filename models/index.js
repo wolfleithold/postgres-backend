7a6e8d351f44bd166dd710a82550f026268c7e70
Wolfgang Leithold <wolfleithold@gmail.com>
2024-07-27 20:23:27 -0700

    updated product tag and tag models

--- models/index.js
+++ models/index.js
@@ -1,20 +1,31 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Category = require("./Category");
+const Product = require("./Product");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// Define associations
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
-  Product,
   Category,
+  Product,
   Tag,
   ProductTag,
 };
